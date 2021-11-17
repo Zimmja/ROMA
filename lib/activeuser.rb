64c92f24a3e35cf06dc57b7cbd51beb5daae6c70
Matt Zimmer <zimmermja@gmail.com>
2021-11-17 12:04:32 +0000

    added login and logout functionality

--- lib/activeuser.rb
+++ lib/activeuser.rb
@@ -18,6 +18,12 @@ class ActiveUser
       @@user_id
     end
 
+    def request_login(username = nil, password = nil)
+      return false if (username.nil? || password.nil?)
+      user_id = find_user_id(username, password)
+      user_id.to_i < 0 ? false : login(user_id)
+    end
+
     def logout
       @@user_id = nil
     end
@@ -32,6 +38,11 @@ class ActiveUser
     end
 
     private
+
+    def find_user_id(username, password)
+      user_row = connection.query("SELECT * FROM users WHERE username = '#{username}' AND password = '#{password}';").first
+      user_row.nil? ? -1 : user_row['id']
+    end
 
     def create(user, pword, mail)
       connection.exec_params("INSERT INTO users (username,password,email) VALUES ($1,$2,$3) RETURNING id", [user,pword,mail])
--- spec/activeuser_spec.rb
+++ spec/activeuser_spec.rb
@@ -16,4 +16,23 @@ describe ActiveUser do
       expect(ActiveUser.id).to eq nil
     end
   end
+
+  describe '.request_login' do
+    it 'prevents login if no username or password are provided' do
+      ActiveUser.logout
+      expect(ActiveUser.request_login).to eq false
+    end
+
+    it 'prevents login if incorrect username and password are provided' do
+      ActiveUser.logout
+      expect(ActiveUser.request_login('Batman','nanananananapassword!')).to eq false
+    end
+
+    it 'allows login with correct details' do
+      ActiveUser.logout
+      ActiveUser.signup('Superman', 'notsuperman', 'klarkkent@gmail.com')
+      ActiveUser.logout
+      expect(ActiveUser.request_login('Superman','notsuperman')).not_to eq false
+    end
+  end
 end
