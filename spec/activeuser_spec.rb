1311b60ed4fec58d2c03d89948021c994b4eca46
Matt Zimmer <zimmermja@gmail.com>
2021-11-17 16:35:17 +0000

    improved ActiveUser class rspec test

--- spec/activeuser_spec.rb
+++ spec/activeuser_spec.rb
@@ -5,7 +5,7 @@ require 'activeuser'
 describe ActiveUser do
   describe '.signup' do
     it '@@user_id is set to the ID of a newly-created user' do
-      id_value = ActiveUser.signup('Bugs Bunny', 'carrot7', 'whatsupdoc@gmail.com')
+      id_value = ActiveUser.signup('Peter Parker', 'spiderman', 'parker@gmail.com')
       expect(ActiveUser.id).to eq id_value
     end
   end
@@ -19,20 +19,15 @@ describe ActiveUser do
 
   describe '.request_login' do
     it 'prevents login if no username or password are provided' do
-      ActiveUser.logout
       expect(ActiveUser.request_login).to eq false
     end
 
     it 'prevents login if incorrect username and password are provided' do
-      ActiveUser.logout
-      expect(ActiveUser.request_login('Batman','nanananananapassword!')).to eq false
+      expect(ActiveUser.request_login('Bruce Wayne','batman')).to eq false
     end
 
     it 'allows login with correct details' do
-      ActiveUser.logout
-      ActiveUser.signup('Superman', 'notsuperman', 'klarkkent@gmail.com')
-      ActiveUser.logout
-      expect(ActiveUser.request_login('Superman','notsuperman')).not_to eq false
+      expect(ActiveUser.request_login('Tony Stark','ironman')).not_to eq false
     end
   end
 end
