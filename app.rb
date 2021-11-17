639c275eaf4ced45429eabcb23c4f7e7cebd42c6
roshanabey2 <roshanabey2@gmail.com>
2021-11-17 12:23:40 +0000

    fixed up index.erb and signup.erb

--- app.rb
+++ app.rb
@@ -17,18 +17,27 @@ class Roma < Sinatra::Base
     erb(:index)
   end
 
+  get '/signup' do
+    erb(:signup)
+  end
 
-  post '/spaces' do
-  #  p ENV
-  #  @spaces = Spaces[blah_user_id_blah].all #work in progress
-  #  erb :'spaces'
-  #end
+  post '/signup/new' do
+    ActiveUser.signup(params[:username], params[:pwd], params[:email])
+    @username = params[:username]
+    redirect to '/spaces'
+  end 
+
+
+  get '/spaces' do
+    @username = ActiveUser.username
+    erb(:spaces)
+  end
 
  # post '/addspace' do
  #   p params[:name]
  #   p params[:fk_user]
  #   Spaces.create(title: params[:title], url: params[:url])
- #   redirect '/spaces'
+ #   redirect '/
  # end
   
   # start the server if ruby file executed directly
