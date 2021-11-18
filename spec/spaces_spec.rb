34866f70b3e20ff1dead04da23221060f5cc9bde
Matt Zimmer <zimmermja@gmail.com>
2021-11-18 11:27:04 +0000

    fixed rspec errors

--- spec/spaces_spec.rb
+++ spec/spaces_spec.rb
@@ -8,16 +8,20 @@ describe Space do
 
   describe '.create' do
     it 'fails if the passed in user ID is nil' do
-      expect { Space.create(nil, 'Stark Tower', '200') }.to raise_error 'User not logged in'
+      expect { Space.create(nil, 'Stark Tower', '200', 
+        'Highrise in central Manhatten', '$2000') }.to raise_error 'User not logged in'
     end
 
     it 'fails if the passed in user ID is not available' do
-      expect { Space.create(1, 'Stark Tower', '250') }.to raise_error 'User not logged in'
+      expect { Space.create(1, 'Stark Tower', '250', 
+        'Highrise in central Manhatten', '$2000') }.to raise_error 'User not logged in'
     end
 
     it 'creates a new space if a valid user ID is passed' do
-      expect { Space.create(user_ids[0], 'Stark Tower', '300') }.to change { Space.all.count }.by(1)
-      expect { Space.create(user_ids[4], 'Asgard', '10000') }.to change { Space.all.count }.by(1)
+      expect { Space.create(user_ids[0], 'Stark Tower', '300', 
+        'Highrise in central Manhatten', '$2000') }.to change { Space.all.count }.by(1)
+      expect { Space.create(user_ids[4], 'Asgard', '10000', 
+        'Another planet entirely', '$50000') }.to change { Space.all.count }.by(1)
     end
   end
 end
